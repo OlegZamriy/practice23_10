4eb82fa24b9ea3361721d2b6bc10b607cb40d120
lysenko_v <lysenko_v@itstep.academy>
2023-10-23 20:07:43 +0300

    1

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
-#include <Windows.h>
-
+#include <ctime> 
+#include <cstdlib> 
 int main() {
-    SetConsoleOutputCP(1251);
-    
+    const int arraySize = 10; 
+    int arr[arraySize];
 
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    for (int i = 0; i < arraySize; i++) {
+        arr[i] = std::rand() % 100; 
+    }
+
+    std::cout << "Array: ";
+    for (int i = 0; i < arraySize; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int minElement = arr[0];
+    int maxElement = arr[0];
+
+    for (int i = 1; i < arraySize; i++) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+        }
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    std::cout << "Minimal element: " << minElement << std::endl;
+    std::cout << "Maximum element: " << maxElement << std::endl;
 
     return 0;
 }
+
