328d2fe503c0925bd41a6279e1c1425e601d1d64
lysenko_v <lysenko_v@itstep.academy>
2023-10-23 19:54:38 +0300

    1

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,23 @@
-#include
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main() {
+    SetConsoleOutputCP(1251);
+    const int n = 5;
+
+    float sides[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Введіть довжину сторони " << i + 1 << ": ";
+        cin >> sides[i];
+    }
+
+    float perimeter = 0;
+    for (int i = 0; i < n; i++) {
+        perimeter += sides[i];
+    }
+
+    cout << "Периметр п'ятикутника: " << perimeter << endl;
+
+    return 0;
+}
